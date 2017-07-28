5144afff249068696cac53a7d287cd9068ef1118
Shane <IAmNatch@users.noreply.github.com>
2017-07-28 14:45:27 -0400

    Started Processor!

---
+++ Development/Functional Backend/processor.js
@@ -0,0 +1,32 @@
+let inputArray = [process.argv[2], process.argv[3], process.argv[4], process.argv[5], process.argv[6]];
+
+function processor (inputArray) {
+    if (inputArray[0] === 'tooltip') {
+        toolTipGenerator(inputArray);
+    }
+
+    else if (inputArray[0] === 'change-css') {
+        changeCSSGenerator(inputArray);
+    }
+
+    else if (inputArray[0] === 'import') {
+        tooltipGenerator(inputArray);
+    }
+
+    else if (inputArray[0] === 'sort') {
+        sortGenerator(inputArray);
+    }
+
+    else {
+        console.log('The function "' + inputArray[0] + '" is not included in our feature set! Please try again!');
+    }
+}
+
+function toolTipGenerator(inputArray) {
+    if (inputArray[1] === 'popup') {
+        TTPopup(inputArray);
+    }
+    else if (inputArray[1] === 'dropdown') {
+        console.log('Sorry! dropdowns have not yet been added to our tooltipping feature set!')
+    }
+}
--- Development/Testing Website/js/app.js
+++ Development/Testing Website/js/app.js
@@ -1,6 +1,3 @@
-let myClassState = $('.target1').css('display');
-
-
 // This is a custom click toggle. It needs to be added for
 //certain fucntions, but only once.
 jQuery.fn.clickToggle = function(a,b) {
@@ -8,7 +5,10 @@ jQuery.fn.clickToggle = function(a,b) {
     return this.on('click', cb);
 };
 
-//
+//Records State of the target element.
+let myClassState = $('.target1').css('display');
+
+//The actual onClick Function. Display none to Display: whatever it used to be.
 $( '.button1' ).clickToggle(
     function() {
         //
