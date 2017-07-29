0647ca6893c4ba82a97454afcecab3d64de2051e
IAmNatch <IAmNatch@users.noreply.github.com>
2017-07-29 13:24:40 -0400

    Added Dropdowns!

--- Development/Functional Backend/Module/tooltipModule.js
+++ Development/Functional Backend/Module/tooltipModule.js
@@ -2,7 +2,8 @@ let tooltip = {
 
     tooltipGenerator: function toolTipGenerator(inputArray) {
         if (inputArray[1] === 'popup') {
-            ttPopup(inputArray);
+            let result = ttClick(inputArray);
+            return result;
         }
         else if (inputArray[1] === 'dropdown') {
             console.log('Sorry! dropdowns have not yet been added to our tooltipping feature set!')
@@ -10,8 +11,8 @@ let tooltip = {
     }
 };
 
-function ttPopup(inputArray) {
-    
+function ttClick(inputArray) {
+
 }
 
 
--- Development/Testing Website/js/app.js
+++ Development/Testing Website/js/app.js
@@ -16,6 +16,7 @@ $( '#tt-trigger1' ).clickToggle(
         $('#tt-target1').css('display', 'none');
     });
 
+// TT Popup on hover
 $('#tt-trigger2').on({
     'mouseenter':function(){
         $('#tt-target2').css('display', myClassState)
@@ -27,14 +28,50 @@ $('#tt-trigger2').on({
 //Tooltip on Scroll
 //Show's Current Scroll Positon
 $(document).scroll(function () {
-    $('#scroll-position').text($(this).scrollTop());
+    $('.scroll-position').text($(this).scrollTop());
 });
 
 $(document).scroll(function () {
     let y = $(this).scrollTop();
-    if (y > 100) {
+    if (y > 100 && y < 180 ) {
         $('#tt-target3').fadeIn();
-    } else {
+    }
+    else {
         $('#tt-target3').fadeOut();
+    }
+});
+
+
+// Tooltip Dropdown on Click
+$( '#tt-trigger4' ).clickToggle(
+    function() {
+        let h = $('#tt-target4').css('height');
+        $('#tt-target4').css('top', '0px');
+    }, function() {
+        let h = $('#tt-target4').css('height');
+        $('#tt-target4').css('top', '-' + h);
+    });
+
+// Tooltip Dropdown Hover
+$('#tt-trigger5').on({
+    'mouseenter':function(){
+        let h = $('#tt-target4').css('height');
+        $('#tt-target5').css('top', '0px');
+    },'mouseleave':function(){
+        let h = $('#tt-target4').css('height');
+        $('#tt-target5').css('top', '-' + h);
+    }
+});
+
+//Tooltip Dropdown on Hover
+
+$(document).scroll(function () {
+    let y = $(this).scrollTop();
+    if (y > 200) {
+        let h = $('#tt-target6').css('height');
+        $('#tt-target6').css('top', '0px');
+    } else {
+        let h = $('#tt-target6').css('height');
+        $('#tt-target6').css('top', '-' + h);
     }
 });
