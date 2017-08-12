201e4f35868eb24f7aa1135ca1a007aee3af1286
Rebecca Vandop <rebeccavandop@Rebeccas-MacBook-Pro.local>
2017-08-12 12:13:41 -0400

    questions for scroll from are on react app

--- js-react-app/src/App.js
+++ js-react-app/src/App.js
@@ -38,43 +38,68 @@ class FlavorForm extends React.Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit} className="nl-form">
+      <form onSubmit={this.handleSubmit} className="nl-form" >
         <label>
-          Pick your favorite La Croix flavor:
+          What are are you trying to create: 
           <select value={this.state.value.question1} onChange={this.handleChange}>
-            <option value="grapefruit">Grapefruit</option>
-            <option value="lime">Lime</option>
-            <option value="coconut">Coconut</option>
-            <option value="mango">Mango</option>
+            <option value="grapefruit">  my event</option>
+            <option value="lime"> tooltip</option>
+            <option value="coconut"> class name change</option>
+            <option value="mango"> sort</option>
           </select>
       </label>
        <br />
-        <label> What would you like to do?
+        <label> What type of tooltip are you trying to create? 
           <select value={this.state.value.question2} onChange={this.handleChange}>
-            <option value="tooltip">tooltip</option>
-            <option value="change-class">change class</option>
-            <option value="drop-down">drop down</option>
+            <option value="tooltip">type</option>
+            <option value="change-class">popup</option>
+            <option value="drop-down">dropdown</option>
           </select>
         </label>
        <br />
-        <label> That is activated:
+        <label> How would you like it to be triggered on:
         	<select value={this.state.value.question3} onChange={this.handleChange}>
-						<option value="on" >on</option>
-						<option value="hover">Hover</option>
-						<option value="click">Click</option>
-						<option value="scroll">Scroll</option>
+						<option value="on" > trigger type</option>
+						<option value="hover">hover</option>
+						<option value="click">click</option>
+						<option value="scroll">click toggle</option>
+            <option value="scroll">scroll</option>
 					</select>
         </label>
       <br />
         <label>
-          Pick your favorite color:  
-          <select value={this.state.value.question4} onChange={this.handleChange}>
-            <option value="yellow"> yellow</option>
-            <option value="green">green</option>
-            <option value="white">white</option>
-            <option value="orange">orange</option>
-          </select>
+          What is your TRIGGER ID or Class: 
+          <input type="text" value={this.state.text}/>
       </label>
+        <br />
+      
+        <label>
+          What is your TARGET ID or Class: 
+          <input type="text" value={this.state.text}/>
+      </label>
+        <br />
+        <label> How would you like to make is disapear:
+        	<select value={this.state.value.question3} onChange={this.handleChange}>
+						<option value="on" > options</option>
+						<option value="hover">opacity</option>
+						<option value="click">dislay: none</option>
+					</select>
+        </label>
+        <br />
+        <label> What Display value would you like to use?
+        	<select value={this.state.value.question3} onChange={this.handleChange}>
+						<option value="on" > options</option>
+						<option value="hover">block</option>
+						<option value="click">inline</option>
+            <option value="hover">inline-block</option>
+						<option value="click">flex</option>
+					</select>
+        </label>
+        <br />
+        <label> Starting and ending(optional) value for Scroll ? <br />
+          <span>Starting value: <input type="text" value={this.state.text}/></span> <br />
+          <span>Ending value (optional): <input type="text" value={this.state.text}/></span>
+        </label>
         <br />
          <button className="nl-submit" type="submit">submit</button>
       </form>
