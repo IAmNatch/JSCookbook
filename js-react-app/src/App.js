51b725ae9b8c1ece54bfc7999c2e568de8fb6483
Rebecca Vandop <rebeccavandop@Rebeccas-MacBook-Pro.local>
2017-08-13 20:26:09 -0400

    added more conditionals for when someone selects drop down. also new form renders when you select the chnage class option :)

--- js-react-app/src/App.js
+++ js-react-app/src/App.js
@@ -7,134 +7,101 @@ class App extends Component {
     return (
       <div className="App">
        <h1> FORM THAT LOGS TO THE CONSOLE</h1>
-       <Test />
+       <Form />
       </div>
     );
   }
 }
 
+class Form extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+        formValues: {}
+    }
 
-// class FlavorForm extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//         type: '',
-//         tooltip: ''
+}
 
-//     };
+handleChange(event) {
+  event.preventDefault();
+  let formValues = this.state.formValues;
+  let name = event.target.name;
+  let value = event.target.value;
 
-//     // this.handleChangeType = this.handleChangeType.bind(this);
-//     this.handleChangeTooltip = this.handleChangeTooltip.bind(this);
-//     // this.handleChange = this.handleChange.bind(this);
-//     this.handleSubmit = this.handleSubmit.bind(this);
-//   }
+  formValues[name] = value;
+  
+  this.setState({formValues})
+  console.log(this.state.formValues["event-type"] )
+}
 
-//   // handleChangeType(event) {
-//   //   // this.setState({value: event.target.value});
-//   //   //  var newArray = this.state.value.type.slice();
-//   //   // newArray.push(event.target.value);
-//   //   this.setState({
-//   //         type:  event.target.type
-//   //   })
-//   // }
-//   // handleChangeTooltip(event){
-//   //   this.setState({
 
-//   //         tooltip: event.target.tooltip
+  render() {
+    return (
+      <div className="App">
+    <form className="nl-form">
+          <label>
+          What are are you trying to create: 
+          <select  name="event-type" value={this.state.formValues["event-type"]} onChange={this.handleChange.bind(this)}>
+            <option value="do-not-submit">  my event</option>
+            <option value="tooltip"> tooltip</option>
+            <option value="change-class-name"> change class name</option>
+            <option value="sort"> sort</option>
+          </select>
+      </label>
+      </form>
+      {this.state.formValues["event-type"] === 'tooltip' ? <Tooltip /> : <div></div>}
+      {this.state.formValues["event-type"] === 'change-class-name' ? <ChangeClass/> : <div></div>}
+      </div>
+    );
+  }
+}
 
-//   //   })
-//   // }
-//   handleChangeTooltip(event){
-//     this.setState({
+class ChangeClass extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+        formValues: {}
+    }
 
-//       tooltip: event.target.tooltip
+}
 
-//     })
-//   }
+handleChange(event) {
+    event.preventDefault();
+    let formValues = this.state.formValues;
+    let name = event.target.name;
+    let value = event.target.value;
+  
+    formValues[name] = value;
+    
+    this.setState({formValues})
+    console.log(this.state.formValues["event-type"] )
+}
 
-//   handleSubmit(event) {
-//    console.log(JSON.stringify(this.state));
-//     event.preventDefault();
-//   }
+handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state.formValues);
+}
 
-//   render() {
-//     return (
-//       <form onSubmit={this.handleSubmit} className="nl-form" >
-//         <label>
-//           What are are you trying to create:
-//           <select value={this.state.type} onChange={this.handleChange}>
-//             <option value="do-not-submit">  my event</option>
-//             <option value="tooltip"> tooltip</option>
-//             <option value="change-class-name"> change class name</option>
-//             <option value="sort"> sort</option>
-//           </select>
-//       </label>
-//        <br />
-//         <label> What type of tooltip are you trying to create?
-//           <select value={this.state.tooltip} onChange={this.handleChangeTooltip}>
-//             <option value="do-not-submit">type</option>
-//             <option value="popups">popup</option>
-//             <option value="dropdown">dropdown</option>
-//           </select>
-//         </label>
-//        <br />
-//         <label> How would you like it to be triggered on:
-//         	<select value={this.state.value} onChange={this.handleChange}>
-// 						<option value="do-not-submit" > trigger type</option>
-// 						<option value="hover">hover</option>
-// 						<option value="click">click</option>
-// 						<option value="click toggle">click toggle</option>
-//             <option value="scroll">scroll</option>
-// 					</select>
-//         </label>
-//       <br />
-//         <label>
-//           What is your TRIGGER ID or Class:
-//           <input type="text" value={this.state.text} required/>
-//       </label>
-//         <br />
+render(){
+  return (
+    <form onSubmit={this.handleSubmit.bind(this)} className="nl-form">
+        <label>
+        What are are you trying to create: 
+        </label>
+      <br />
+      <button className="nl-submit" type="submit">submit</button>
+       </form>
+  )
+}
+}
 
-//         <label>
-//           What is your TARGET ID or Class:
-//           <input type="text" value={this.state.text} required/>
-//       </label>
-//         <br />
-//         <label> How would you like to make is disapear:
-//         	<select value={this.state.value} onChange={this.handleChange}>
-// 						<option value="do-not-submit" > options</option>
-// 						<option value="opacity">opacity</option>
-// 						<option value="dislay: none">dislay: none</option>
-// 					</select>
-//         </label>
-//         <br />
-//         <label> What Display value would you like to use?
-//         	<select value={this.state.value} onChange={this.handleChange}>
-// 						<option value="do-not-submit" > options</option>
-// 						<option value="block">block</option>
-// 						<option value="inline">inline</option>
-//             <option value="inline-block">inline-block</option>
-// 						<option value="flex">flex</option>
-// 					</select>
-//         </label>
-//         <br />
-//         <label> Starting and ending(optional) value for Scroll ? <br />
-//           <span>Starting value: <input type="text" value={this.state.text} required/></span> <br />
-//           <span>Ending value (optional): <input type="text" value={this.state.text}/></span>
-//         </label>
-//         <br />
-//          <button className="nl-submit" type="submit">submit</button>
-//       </form>
-//     );
-//   }
-// }
-class Test extends React.Component {
+class Tooltip extends React.Component {
   constructor(props) {
       super(props)
       this.state = {
           formValues: {}
       }
 
-      seeSomething: false;
   }
 
   handleChange(event) {
@@ -196,7 +163,7 @@ class Test extends React.Component {
           <input type="text" name="target-id-class" value={this.state.formValues["target-id-class"]} onChange={this.handleChange.bind(this)} />
       </label>
       <br />
-        <label style={{display: this.state.formValues["target-id-class"] &&  this.state.formValues["event-type"] === 'tooltip' ? 'inline-block' : 'none' }}> How would you like to make is disapear:
+        <label style={{display: this.state.formValues["target-id-class"] &&  this.state.formValues["event-type"] === 'tooltip'  && this.state.formValues["type"] != 'dropdown' ? 'inline-block' : 'none' }}> How would you like to make is disapear:
         	<select name="disapaer" value={this.state.formValues["disapaer"]} onChange={this.handleChange.bind(this)}>
 			
     			<option value="do-not-submit" > options</option>
@@ -205,7 +172,7 @@ class Test extends React.Component {
 					</select>
         </label>
         <br />
-        <label style={{display: this.state.formValues["disapaer"] &&  this.state.formValues["event-type"] === 'tooltip' ? 'inline-block' : 'none' }}>  What Display value would you like to use?
+        <label style={{display: this.state.formValues["disapaer"] &&  this.state.formValues["event-type"] === 'tooltip'  && this.state.formValues["type"] != 'dropdown'  ? 'inline-block' : 'none' }}>  What Display value would you like to use?
         	<select name="disapaer-value" value={this.state.formValues["disapaer-value"]} onChange={this.handleChange.bind(this)}>
 						<option value="do-not-submit" > options</option>
 						<option value="block">block</option>
@@ -215,12 +182,12 @@ class Test extends React.Component {
 					</select>
         </label>
         <br />
-        <label style={{display:  this.state.formValues["disapaer-value"]  &&  this.state.formValues["event-type"]  === 'tooltip' ? 'inline-block' : 'none' }}> Starting and ending(optional) value for Scroll ? <br />
-          <span>Starting value: <input type="text" name="scroll-start"  value={this.state.formValues["scroll-start"]} onChange={this.handleChange.bind(this)} required/></span> <br />
+        <label style={{display:  this.state.formValues["disapaer-value"]  &&  this.state.formValues["event-type"]  === 'tooltip'  && this.state.formValues["type"] != 'dropdown'  ? 'inline-block' : 'none' }}> Starting and ending(optional) value for Scroll ? <br />
+          <span>Starting value: <input type="text" name="scroll-start"  value={this.state.formValues["scroll-start"]} onChange={this.handleChange.bind(this)} /></span> <br />
           <span>Ending value (optional): <input type="text" name="scroll-end"  value={this.state.formValues["scroll-end"]} onChange={this.handleChange.bind(this)} /></span>
         </label>
         <br />
-        <button style={{display:  this.state.formValues["scroll-start"]  ? 'inline-block' : 'none' }} className="nl-submit" type="submit">submit</button>
+        <button style={{display:  (this.state.formValues["target-id-class"] && this.state.formValues["type"] == 'dropdown') || this.state.formValues["scroll-start"] ? 'inline-block' : 'none' }} className="nl-submit" type="submit">submit</button>
          </form>
     )
   }
